9a6ab7a9cea6b1695e2aff17fb6b02d4732009f9
yager <danila.kirillov@list.ru>
2026-03-06 01:42:06 +0400

    Third commit

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+from functools import wraps
+from typing import Callable
 import os
 import time
 import sys
@@ -17,8 +19,12 @@ load_dotenv()
 
 logger = logging.getLogger(__name__)
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_USERNAME = "batzatbot"
 SAVE_INTERVAL = 60
 USERS_FILE = "users.json"
+CHATS_FILE = "chats.json"
+FACTS_FILE = "facts.json"
+BAD_WORDS_FILE = "bad_words.json"
 
 cons = ['б', 'в', 'г', 'д', 'ж', 'з', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ']
 vowels = ['а', 'е', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
@@ -27,17 +33,16 @@ dop_prefixes = ["гр", "р", "вр", "вз", "бр", "фр", "ср", "тр"]
 two_symbols = ["нь", "бь", "дь", "мь", "зь", "фь", "ль"]
 postfixes = ["я", "и"]
 digital_words = ["84471", "7171", "6161", "6961", "7971"]
-bad_words = ["Никита пидарас @Xonalz", "Антон жидкий @antonykozh", "У Антона спина белая @antonykozh", "Артура ебали @ArturBardur", "У Никикты говно в штанах @Xonalz", "Хуй", "Алмазный тунг тунг сахур", "Жопа"]
+swear_shards = ["ебл", "пидо", "пидар", "хуй", "пизд", "тупой", "дура", "кончен", "гондон", "шлюх", "суки", "сука", "уебище"]
 
 dp = Dispatcher()
-active_chats = set()
 last_save_time = time.time()
 
 
-def load_users() -> dict:
-    if Path(USERS_FILE).exists():
+def load_data(file) -> dict:
+    if Path(file).exists():
         try:
-            with open(USERS_FILE, 'r', encoding="utf-8") as f:
+            with open(file, 'r', encoding="utf-8") as f:
                 data = json.load(f)
                 return data if isinstance(data, dict) else {}
         except (json.JSONDecodeError, IOError):
@@ -45,39 +50,31 @@ def load_users() -> dict:
     return {}
 
 
-users_cache = load_users()
+def save_chats():
+    try:
+        with open(CHATS_FILE, 'w', encoding='utf-8') as f:
+            json.dump({"active_chats": list(active_chats)}, f, ensure_ascii=False, indent=2)
+    except Exception as e:
+        logger.error(f"Ошибка сохранения чатов: {e}")
+
+
+users_cache = load_data(USERS_FILE)
 users_cache_len = len(users_cache)
-
-
-def collect_user(message):
-    user = message.from_user
-    user_id = str(user.id)
-    if user_id not in users_cache:
-        user_data = {
-            'id': user.id,
-            'username': user.username,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'language': user.language_code,
-            'first_seen': str(message.date),
-            'chat_id': message.chat.id,
-            'chats': [message.chat.id]
-        }
-        users_cache[user_id] = user_data
-    else:
-        if message.chat.id not in users_cache[user_id]['chats']:
-            users_cache[user_id]['chats'].append(message.chat.id)
+active_chats_dict = load_data(CHATS_FILE)
+active_chats = set(active_chats_dict.get("active_chats", []))
+facts = load_data(FACTS_FILE)
+bad_words = load_data(BAD_WORDS_FILE)
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """
     Command /start
-    """ 
-    collect_user(message)
+    """     
     chat_id = message.chat.id
-    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}! Бот запущен и готов!")   
-    active_chats.add(chat_id)
+    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}! Бот запущен и готов!")
+    active_chats.add(chat_id)  
+    save_chats()
 
 
 @dp.message(Command("stop"))
@@ -88,6 +85,7 @@ async def command_stop_handler(message: Message):
     chat_id = message.chat.id
     if chat_id in active_chats:
         active_chats.discard(chat_id)
+        save_chats()
         await message.answer(f"Бот остановлен! Для возобновления - напишите /start")
         logger.info(f"Бот остановлен в чате: {chat_id}")
     else:
@@ -96,19 +94,8 @@ async def command_stop_handler(message: Message):
 
 def insult_call(user):
     name = user["first_name"]
-    username = user["username"]
-    facts = [
-        f"🚨 ВНИМАНИЕ! 🚨\n\nПидарас найден -> {name} (@{username})",
-        f"🚨 ЭКСТРЕННОЕ ОБЪЯВЛЕНИЕ! 🚨\n\nГлавный долбоёб этого чата — {name} (@{username})",
-        f"🤡 ГЛАВНЫЙ ПЕТУХ ЧАТА 🤡\n\nПо результатам голосования побеждает {name} (@{username})! Поздравляем, ты сосал больше всех",
-        f"💀 ТРАГЕДИЯ 💀\n\nУ {name} (@{username}) вместо мозгов — говно. Врачи разводят руками: 'Такое дерьмо мы видим впервые'",
-        f"🏆 ПОЧЕТНОЕ ЗВАНИЕ 🏆\n\n{name} (@{username}) присвоено звание 'Заслуженный хуесос года'! Жена гордится, соседи завидуют",
-        f"🧻 ТУАЛЕТНЫЕ ИСТОРИИ 🧻\n\n{name} (@{username}) сегодня не вытер жопу",
-        f"🏆 ЗАСЛУЖЕННЫЙ ХУЙ 🏆\n\nПоздравляем {name} (@{username})! Ты выиграл хуй в рот. Носи с гордостью, мразь.",
-        f"🚽 АНАЛЬНАЯ КАТАСТРОФА 🚽\n\nУ {name} (@{username}) сегодня праздник: он родился. Все остальные в ужасе, врачи в шоке, мать в слезах, что такое чудовище выжило.",
-        f"💩 ГОВНОМЕС 💩\n\n{name} (@{username}) — бесполезный, вонючий и только место занимает, мудак.",
-    ]
-    return random.choice(facts)
+    username = user["username"]    
+    return random.choice(facts["facts"]).format(name=name, username=username)
 
 
 @dp.message(Command("call"))
@@ -130,9 +117,77 @@ async def command_fact_handler(message: Message) -> None:
     await message.answer(insult_call(users_cache[user_id]))
 
 
+def collect_user(message: Message):
+    user = message.from_user
+    user_id = str(user.id)
+    if user_id not in users_cache:
+        user_data = {
+            'id': user.id,
+            'username': user.username,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'language': user.language_code,
+            'first_seen': str(message.date),
+            'chat_id': message.chat.id,
+            'chats': [message.chat.id]
+        }
+        users_cache[user_id] = user_data
+    else:
+        if message.chat.id not in users_cache[user_id]['chats']:
+            users_cache[user_id]['chats'].append(message.chat.id)
+
+
+def random_answer(message: Message):
+    if message.text and not message.text.startswith('/') and not message.from_user.is_bot:        
+        if '?' in message.text:
+            if random.random() < 0.3:
+                answers = [
+                    "А сам как думаешь?",
+                    "Хуй его знает",                   
+                    "Спроси у мамы",
+                    "42",
+                    "52",
+                    "67",
+                    "Гугли, чё",
+                    "Ты у кого спрашиваешь?",
+                    "🤷‍♂️"
+                ]
+                return random.choice(answers)                
+        elif f"@{BOT_USERNAME}" in message.text:
+            return "Че надо?"
+    return None
+        
+
+def swear_answer(message: Message):
+    if message.text and not message.text.startswith("/") and not message.from_user.is_bot:
+        if any(swear in message.text for swear in swear_shards):
+            if random.random() < 0.3:
+                answers = [
+                        "Нахуй ты ругаешься?",
+                        "Ты еблан тупой!",                  
+                        "Пошел ты нахуй!",
+                        "Похуй ваще...",
+                        "Ты пидр",
+                        "Антон шл**а",
+                        "Д пошли вы нахуй!"
+                    ]
+                return random.choice(answers)
+    return None
+                    
+
 @dp.message()
-async def collect_user_info(message: Message):
+async def handle_all_messages(message: Message):
     collect_user(message)
+
+    question_reply = random_answer(message)
+    if question_reply:
+        await message.reply(question_reply)
+        return
+      
+    swear_reply = swear_answer(message)
+    if swear_reply:
+        await message.reply(swear_reply)
+        return
 
 
 async def collect_periodic_users():
@@ -156,20 +211,19 @@ async def collect_periodic_users():
             await asyncio.sleep(5)
         
 
-def random_words():    
-    pattern = fr"^[{''.join(cons)}][{''.join(vowels)}][{''.join(cons)}]ь$"    
+def random_words(chat_id=None):    
     word = random.choice(cons) + random.choice(vowels) + random.choice(cons)
 
     r = random.random()
-    if r < 0.01:
-        word = random.choice(bad_words)
-    elif r < 0.02:
+    if r < 0.1:
+        word = random.choice(bad_words["bad_words"])
+    elif r < 0.11:
         word = random.choice(digital_words)
-    elif r < 0.03:
+    elif r < 0.12:
         word = random.choice(two_symbols)
-    elif r < 0.1:
+    elif r < 0.13:
         word = random.choice(dop_prefixes) + word[1:] + 'ь'
-    elif r < 0.15:
+    elif r < 0.14:
         word = random.choice(prefixes) + word + 'ь'
     elif r < 0.2:
         word += random.choice(postfixes)    
@@ -179,30 +233,44 @@ def random_words():
     return word
 
 
-async def periodic_messages_sender(bot: Bot):
+def random_insult(chat_id):
+    chat_users = []
+    for uid, data in users_cache.items():
+        if chat_id in data.get('chats', []) and uid != "7743054853":
+            chat_users.append(uid)
+    user_id = random.choice(chat_users)
+    user = users_cache[user_id]
+    name = user["first_name"]
+    username = user["username"]
+    return f"🚨 ВНИМАНИЕ! 🚨\n\nГлавный пидарас дня -> {name} (@{username})"
+
+
+async def periodic_messages_sender(bot: Bot, interval: int, func: Callable):
     while True:
         try:
             if active_chats:
                 for chat_id in list(active_chats):
                     try:
-                        await bot.send_message(chat_id, random_words())    
+                        await bot.send_message(chat_id, func(chat_id))    
                         await asyncio.sleep(3)
                     except Exception as e:
                         if "bot was blocked" in str(e).lower():
                             logger.warning(f"Бот заблокирован в чате {chat_id}, удаляю")
                             active_chats.discard(chat_id)
+                            save_chats()
                         else:
                             logger.error(f'Ошибка отправки в {chat_id}: {e}')       
         except Exception as e:
             logger.error(f'Ошибка в sender: {e}')         
         finally:
-            await asyncio.sleep(1800)
+            await asyncio.sleep(interval)
 
 
 async def main():
     bot = Bot(BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    asyncio.create_task(periodic_messages_sender(bot))
-    asyncio.create_task(collect_periodic_users())
+    asyncio.create_task(periodic_messages_sender(bot, 900, random_words))
+    asyncio.create_task(periodic_messages_sender(bot, 86400, random_insult))
+    asyncio.create_task(collect_periodic_users())    
     logging.info("Бот запущен!")
     await dp.start_polling(bot)
 
